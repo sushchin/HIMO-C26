8ed5902cb1c68de8ef88bfdfdd3c815658c9274a
Ihor <108277825+sushchin@users.noreply.github.com>
2022-08-09 16:31:27 +0400

    HIMO

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function(){
+
+$('.bike-slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+
+});
+
+$(function(){
+
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    });
